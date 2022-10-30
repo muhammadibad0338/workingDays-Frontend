0c4c85c13dd1fbc2a417712a0fa65c162c998dc0
Muhammad Ibad <mibad0338@gmail.com>
2022-10-30 22:50:58 +0500

    user  Reducer Set

--- src/Redux/User/UserReducer.js
+++ src/Redux/User/UserReducer.js
@@ -1,4 +1,4 @@
-import axios  from "axios";
+import axios from "axios";
 import { baseUrl } from "../baseUrl";
 import {
     SET_ERROR,
@@ -8,3 +8,56 @@ import {
     SET_TOKEN
 } from "./UserTypes";
 
+const initialState = {
+    user: {},
+    loading: false,
+    updating: false,
+    logged: false,
+    error: null,
+    authToken: "",
+    passwordOtp: "",
+    users: [],
+};
+
+const userReducer = (state = initialState, action) => {
+    const { payload, type } = action;
+    switch (type) {
+        case SET_USER:
+            return {
+                ...state,
+                user: payload,
+                authToken: payload.token,
+                updating: false,
+                logged: true,
+                loading: false,
+            };
+        case SET_LOGOUT:
+            return {
+                ...state,
+                user: {},
+                updating: false,
+                logged: false,
+                loading: false,
+            };
+        case SET_ERROR:
+            return {
+                ...state,
+                error: payload,
+            };
+        case SET_TOKEN:
+            return {
+                ...state,
+                authToken: payload,
+            };
+        case SET_LOADING:
+            return {
+                ...state,
+                loading: payload,
+                error: null,
+            };
+        default:
+            return state;
+    }
+};
+
+export default userReducer;
