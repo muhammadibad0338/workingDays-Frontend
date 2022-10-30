33ea033c14785b5a718b4647bbbbe54a7be75ab5
Muhammad Ibad <mibad0338@gmail.com>
2022-10-30 22:44:57 +0500

    user Action Set

--- src/Redux/User/UserReducer.js
+++ src/Redux/User/UserReducer.js
@@ -7,38 +7,4 @@ import {
     SET_USER,
     SET_TOKEN
 } from "./UserTypes";
-import Swal from "sweetalert2";
 
-export const setUser = (user) => {
-    return {
-        type: SET_USER,
-        payload: user,
-    };
-};
-
-export const setLoading = (loading) => {
-    return {
-        type: SET_LOADING,
-        payload: loading,
-    };
-};
-
-export const setError = (error) => {
-    return {
-        type: SET_ERROR,
-        payload: error,
-    };
-};
-
-export const setLogout = () => {
-    return {
-        type: SET_LOGOUT,
-    };
-};
-
-export const setToken = (token) => {
-    return {
-        type: SET_TOKEN,
-        payload: token,
-    };
-};
