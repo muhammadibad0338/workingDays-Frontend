56ae088f76149c2dd71dbfc6e9721d17ef1859a9
Muhammad Ibad <mibad0338@gmail.com>
2022-10-30 21:09:39 +0500

    user Types setting

---
+++ src/Redux/User/UserTypes.js
@@ -0,0 +1,5 @@
+export const SET_USER = "SET_USER";
+export const SET_LOADING = "SET_LOADING";
+export const SET_ERROR = "SET_ERROR";
+export const SET_TOKEN = "SET_TOKEN";
+export const SET_LOGOUT = "USER_SET_LOGOUT";
