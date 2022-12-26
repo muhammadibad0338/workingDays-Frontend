4d3be5fd053267186173c52a657aa55239500e7f
Muhammad Ibad <mibad0338@gmail.com>
2022-12-26 17:22:54 +0500

    projectReducer Set

---
+++ src/Redux/Project/ProjectReducer.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { baseUrl } from "../baseUrl";
+import {
+    SET_ERROR,
+    SET_LOADING,
+    GET_ALL_PROJECTS
+} from "./ProjectTypes";
+
+const initialState = {
+    projects: [],
+    loading: false,
+    updating: false,
+    error: null,
+};
+
+
+const projectReducer = (state = initialState, action) => {
+    const { payload, type } = action;
+    switch (type) {
+        case GET_ALL_PROJECTS:
+            return {
+                ...state,
+                loading: false,
+                projects:payload
+            };
+        case SET_ERROR:
+            return {
+                ...state,
+                error: payload,
+            };
+        case SET_LOADING:
+            return {
+                ...state,
+                loading: payload,
+                error: null,
+            };
+        default:
+            return state;
+    }
+};
+
+export default projectReducer;
