7d1421c076939d3f9129a16dd60e222a7ac2b045
Muhammad Ibad <mibad0338@gmail.com>
2023-01-09 01:20:34 +0500

    getProjectsTasks api action make

--- src/Redux/Task/TaskAction.js
+++ src/Redux/Task/TaskAction.js
@@ -9,21 +9,47 @@ import Swal from "sweetalert2";
 
 export const setLoading = (loading) => {
     return {
-      type: SET_LOADING,
-      payload: loading,
+        type: SET_LOADING,
+        payload: loading,
     };
-  };
-  
-  export const setError = (error) => {
+};
+
+export const setError = (error) => {
     return {
-      type: SET_ERROR,
-      payload: error,
+        type: SET_ERROR,
+        payload: error,
     };
-  };
-  
-  export const setTasks = (tasks) => {
+};
+
+export const setTasks = (tasks) => {
     return {
-      type: GET_ALL_TASKS,
-      payload: tasks
+        type: GET_ALL_TASKS,
+        payload: tasks
     }
-  }
+}
+
+
+export const getProjectsTasks = (id) => async (dispatch) => {
+    try {
+      dispatch(setLoading(true));
+      let res = await axios({
+        method: "GET",
+        url: `${baseUrl}/task/currentProjectTaks/${id}`,
+      });
+      await dispatch(setTasks(res?.data?.tasks))
+      dispatch(setLoading(false));
+    } catch (err) {
+      Swal.fire({
+        customClass: {
+          container: `my-swal`,
+        },
+        icon: "error",
+        title: "Working Days",
+        html: `<strong><font color="black">Something went wrong while getting your Tasks</font></strong>`,
+      });
+      dispatch(setLoading(false));
+      dispatch(setError(err));
+      //   console.log(err,"getCurrentUser")
+    }
+  };
+  
